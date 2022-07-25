2a81a4e0e4326723002b43a2a2536a503397196f
Maxim Zhiburt <zhiburt@gmail.com>
2022-07-25 16:39:59 +0300

    Update examples
    Signed-off-by: Maxim Zhiburt <zhiburt@gmail.com>

--- examples/list.rs
+++ examples/list.rs
@@ -1,25 +1,33 @@
 use tiny_gradient::{Gradient, GradientStr, RGB};
 
 const LOGO: &str = r"
-     _     _                                                       _   _                  _   
-    | |   (_)                                                     | | (_)                | |  
-    | |_   _   _ __    _   _   ______    __ _   _ __    __ _    __| |  _    ___   _ __   | |_ 
-    | __| | | | '_ \  | | | | |______|  / _` | | '__|  / _` |  / _` | | |  / _ \ | '_ \  | __|
-    | |_  | | | | | | | |_| |          | (_| | | |    | (_| | | (_| | | | |  __/ | | | | | |_ 
-     \__| |_| |_| |_|  \__, |           \__, | |_|     \__,_|  \__,_| |_|  \___| |_| |_|  \__|
-                        __/ |            __/ |                                                
-                       |___/            |___/                                                 
+ _     _                                                       _   _                  _   
+| |   (_)                                                     | | (_)                | |  
+| |_   _   _ __    _   _   ______    __ _   _ __    __ _    __| |  _    ___   _ __   | |_ 
+| __| | | | '_ \  | | | | |______|  / _` | | '__|  / _` |  / _` | | |  / _ \ | '_ \  | __|
+| |_  | | | | | | | |_| |          | (_| | | |    | (_| | | (_| | | | |  __/ | | | | | |_ 
+ \__| |_| |_| |_|  \__, |           \__, | |_|     \__,_|  \__,_| |_|  \___| |_| |_|  \__|
+                    __/ |            __/ |                                                
+                   |___/            |___/                                                 
 ";
 
 fn main() {
+    let logo = LOGO
+        .lines()
+        .map(|l| format!("\t\t{}", l))
+        .collect::<Vec<_>>()
+        .join("\n");
     let raibow = [
         RGB::new(255, 0, 0),
         RGB::new(0, 255, 0),
         RGB::new(0, 0, 255),
     ];
-    println!("{}", LOGO.gradient(raibow));
 
-    [
+    println!("{}", logo.gradient(raibow));
+
+    println!();
+
+    let gradients = [
         Gradient::Atlast,
         Gradient::Cristal,
         Gradient::Fruit,
@@ -35,13 +43,13 @@ fn main() {
         Gradient::Vice,
         Gradient::Monsoon,
         Gradient::Forest,
-    ]
-    .into_iter()
-    .for_each(|gradient| {
-        let short = "▇".repeat(10);
-        let short = short.gradient(gradient);
-        let long = "▇".repeat(100);
-        let long = long.gradient(gradient);
+    ];
+
+    gradients.into_iter().for_each(|gradient| {
+        let short = " ".repeat(10);
+        let short = short.gradient(gradient).background();
+        let long = " ".repeat(100);
+        let long = long.gradient(gradient).background();
         let name = format!("{:?}", gradient);
         println!(" {:<15} {} {}", name, short, long);
     });
